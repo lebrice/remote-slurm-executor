781c6ec3565d08da48ab46bbccaaaeedcff7f254
Fabrice Normandin <normandf@mila.quebec>
2024-10-18 12:42:06 -0400

    Tweak test
    Signed-off-by: Fabrice Normandin <normandf@mila.quebec>

--- remote_slurm_executor/integration_test.py
+++ remote_slurm_executor/integration_test.py
@@ -48,7 +48,7 @@ def executor(cluster: str):
     executor = remote_slurm_executor.RemoteSlurmExecutor(
         folder=f"logs/{cluster}/%j",  # todo: perhaps we can rename this folder?
         cluster_hostname=cluster,
-        I_dont_care_about_reproducibility=True,
+        # I_dont_care_about_reproducibility=True,
     )
 
     if cluster != "mila":
